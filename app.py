654d95f91b78fb7d6fabd1dc2803e9bfb51a2833
Vicky Kumar <imvickykumar999@gmail.com>
2020-12-23 23:52:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+UPLOAD_FOLDER = 'uploads'
+try:
+    import os
+    os.system('mkdir uploads')
+except:
+    pass
+    
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
